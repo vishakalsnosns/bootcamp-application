a59cd4db17547c4e092fe684f52d7c8722ec6b6d
Vishal <143796515+vishakalsnosns@users.noreply.github.com>
2025-06-22 23:15:41 +1000

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,4 +1,18 @@
+"""
+Converts a list of digits to a single number without casting or conversion.
+
+Explanation:
+- a number, such as 8351, is made up of 8000+300+50+1.
+- store the output as 0 initially.
+- for each digit from the left, multiply it by 10 to the power of some exponent.
+- for 8351, the leftmost digit x should be 3, which is the length, 4, minus 1.
+- x needs to go from 3 to 2 to 1 to 0, therefore subtract 1 for each iteration of the loop.
+- add each base 10 number procedurally and then print it.
+"""
+
 digits = [8,3,5,1]
+#digits = [1, 1, 1, 1]
+#digits = [9, 1, 4, 5, 1, 2, 5, 6, 3, 1, 4]
 
 output = 0
 
--- task2.py
+++ task2.py
@@ -1,3 +1,12 @@
+"""
+Plays Higher or Lower with the user for integers between 1 and 100.
+
+Explanation:
+- the main loop first gets a validated guess.
+- Then sees if its higher or lower.
+- Then repeats until the number has been guessed. 
+"""
+
 import random
 
 def GetInput():
@@ -10,11 +19,14 @@ def GetInput():
     except ValueError:
         print("Input an integer...")
 
+#The main loop of the game.
 def GuessingGame(goal):
     guess = None
+    #Ensure the guess is valid. A returned value means its validated, see GetInput().
     while not guess:
         guess = GetInput()
 
+    #Check the ranges respective to the goal value. Recursively call the main loop.
     if guess < goal:
         print(f"{guess} is lower than my number.")
         return GuessingGame(goal)
