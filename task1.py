9439cbf955c0af55f34352439001dfd1c76c5e13
Vishal <143796515+vishakalsnosns@users.noreply.github.com>
2025-06-24 19:52:05 +1000

    Add files via upload

--- task1.py
+++ task1.py
@@ -3,23 +3,26 @@ Converts a list of digits to a single number without casting or conversion.
 
 Explanation:
 - a number, such as 8351, is made up of 8000+300+50+1.
-- store the output as 0 initially.
-- for each digit from the left, multiply it by 10 to the power of some exponent.
-- for 8351, the leftmost digit x should be 3, which is the length, 4, minus 1.
-- x needs to go from 3 to 2 to 1 to 0, therefore subtract 1 for each iteration of the loop.
-- add each base 10 number procedurally and then print it.
+- overall strategy is to add up each digit multiplied by a base 10 exponent corresponding to its position.
+
+- reverse the list so that the 0th exponent comes first (10^0 is 1); corresponding to 1st position.
+- as i increases in the for loop, it corresponds to the base ten exponent incrementally.
 """
 
+def Solution(digits):
+    digits.reverse()
+
+    output = 0
+
+    length = len(digits)
+    for i, digit in enumerate(digits):
+        output += digit * 10**(i)
+
+    return output
+
+
 digits = [8,3,5,1]
-#digits = [1, 1, 1, 1]
-#digits = [9, 1, 4, 5, 1, 2, 5, 6, 3, 1, 4]
+# digits = [1, 1, 1, 1]
+# digits = [9, 1, 4, 5, 1, 2, 5, 6, 3, 1, 4]
 
-output = 0
-
-length = len(digits)
-for i, digit in enumerate(digits):
-    exp = length - (i+1)
-
-    output += digit * 10**(exp)
-
-print(output)
+print(Solution(digits))
